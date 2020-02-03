acfdc0355a662aa7e342f6b2dece10f427764205
samgos <samuel.jj.gosling@gmail.com>
2020-02-03 14:23:39 +0000

    QuadraticTotals Schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,10 +2,11 @@ import { newProposalIssued as newProposalIssuedEvent} from "../generated/VotePro
 import { VoteOption } from "../generated/VoteProposalPool/templates"
 import { newProposalIssued } from "../generated/schema"
 import { Address } from "@graphprotocol/graph-ts";
+import { getQuadraticTotals } from "./quadraticTotals.ts";
 
 export function handleFundsSent(event: newProposalIssuedEvent): void {
   let entity = new newProposalIssued(event.transaction.hash.toHex())
-  entity.issuer  = event.params.issuer 
+  entity.issuer  = event.params.issuer
   entity.deadline = event.params.deadline
   entity.name = event.params.name
   entity.data = event.params.data
--- src/mappingsVote.ts
+++ src/mappingsVote.ts
@@ -1,11 +1,15 @@
 import { AnonymousDeposit as AnonymousDepositEvent } from "../generated/VoteProposalPool/templates/VoteOption/VoteOption"
-import { AnonymousDeposit } from "../generated/schema"
+import { AnonymousDeposit, QuadraticTotals } from "../generated/schema"
 
 export function handleAnonymousDeposit(event: AnonymousDepositEvent): void {
   let entity = new AnonymousDeposit(event.transaction.hash.toHex())
-  entity.SenderAddr = event.params.from
-  entity.ContriValue = event.params.value
-  entity.PropName = event.params.name
-  entity.Choice = event.params.option
+  let proposal = QuadraticTotals.load(event.params.name)
+
+  entity.timestamp = event.block.timestamp
+  entity.signaller = event.params.from
+  entity.value = event.params.value
+  entity.proposal = event.params.name
+  entity.choice = event.params.option
+  
   entity.save()
 }
