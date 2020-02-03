620f49c06b536455df540b742839a2ca046af24a
samgos <samuel.jj.gosling@gmail.com>
2020-02-03 16:17:58 +0000

    getQuadraticTotals function for mapping and operation reorg

--- src/mappingsVote.ts
+++ src/mappingsVote.ts
@@ -1,6 +1,6 @@
 import { AnonymousDeposit as AnonymousDepositEvent } from "../generated/VoteProposalPool/templates/VoteOption/VoteOption"
 import { AnonymousDeposit, QuadraticTotals, Burner } from "../generated/schema"
-import { getQuadraticTotals } from "./quadraticTotals.ts";
+import { getQuadraticTotals } from "./quadraticTotals.ts"
 
 export function handleAnonymousDeposit(event: AnonymousDepositEvent): void {
   let transactionHash = event.transaction.hash.toHex()
@@ -10,22 +10,21 @@ export function handleAnonymousDeposit(event: AnonymousDepositEvent): void {
 
   if(proposal === null) proposal = new QuadraticTotals(proposalId)
 
-  let burners = proposals.burners
-  burners.push(transactionHash)
-
-  let quadraticTotals = getQuadraticTotals(burners)
-
-  proposal.burners = burners
-  proposal.decline = quadraticTotals.reject
-  proposal.approve = quadraticTotals.pass
-  proposal.total = quadraticTotals.sum
-
   entity.timestamp = event.block.timestamp
   entity.signaller = event.params.from
   entity.choice = event.params.option
   entity.value = event.params.value
   entity.proposal = proposalId
-
-  proposal.save()
   entity.save()
+
+  let burners = proposal.burners
+  burners.push(transactionHash)
+
+  let quadraticTotals = getQuadraticTotals(burners)
+
+  proposal.decline = quadraticTotals.reject
+  proposal.approve = quadraticTotals.pass
+  proposal.total = quadraticTotals.sum
+  proposal.burners = burners
+  proposal.save()
 }
--- src/quadraticTotals.ts
+++ src/quadraticTotals.ts
@@ -1,3 +1,19 @@
 import { AnonymousDeposit } from "../generated/VoteProposalPool/templates/VoteOption/VoteOption"
 
-export function getQuadraticTotals(burners): void {}
+export function getQuadraticTotals(burners): void {
+  let [ rejectSqrt, passSqrt, totalValue ] = [0, 0, 0]
+
+  for(index in burners){
+    let transactionHash = burners[index]
+    let burn = AnonymousDeposit.load(transactionHash)
+
+    if(burn.choice === "yes") passSqrt = burn.value.pow(0.5) + passSqrt
+    else rejectSqrt = burn.value.pow(0.5) + rejectSqrt
+
+    totalValue = totalValue + burn.value;
+  } return {
+    reject: rejectSqrt,
+    total: totalValue,
+    pass: passSqrt
+  }
+}
