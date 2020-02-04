ee185b8a799bcf445ccea18a35fdc9e1f39361cd
samgos <samuel.jj.gosling@gmail.com>
2020-02-04 23:24:33 +0000

    reformation to mapping process and new schema

--- src/mappingsVote.ts
+++ src/mappingsVote.ts
@@ -1,5 +1,5 @@
 import { AnonymousDeposit as AnonymousDepositEvent } from "../generated/VoteProposalPool/templates/VoteOption/VoteOption"
-import { AnonymousDeposit, QuadraticTotal } from "../generated/schema"
+import { AnonymousDeposit, Proposal, Signaller } from "../generated/schema"
 import { BigInt, BigDecimal } from "@graphprotocol/graph-ts"
 
 function legacyNumber(number: BigInt): number {
@@ -18,44 +18,76 @@ export function handleAnonymousDeposit(event: AnonymousDepositEvent): void {
   entity.value = event.params.value
   entity.save()
 
-  getQuadraticTotals(event.params.name, transactionHash)
+  storeSignallerMetadata(event)
+  storeProposal(event)
 }
 
-function getQuadraticTotals(proposal: string, hash: string): void {
-  let quadratics = QuadraticTotal.load(proposal)
-  let totalValue: number = 0
-  let rejectSqrt: number = 0
-  let passSqrt: number = 0
+function storeSignallerMetadata(event: AnonymousDepositEvent): void {
+  let transactionHash = event.transaction.hash.toHex()
+  let signaller = Signaller.load(event.params.from)
 
-  if(quadratics == null) quadratics = new QuadraticTotal(proposal)
+  if(signaller == null) signaller = new Signaller(event.params.from)
 
-  let burners :string[] = quadratics.burners as Array<string>
+  let total: BigInt = signaller.burned + event.params.value
+  let burns: string[] = signaller.burns
 
+  if(burns == null) burns = new Array()
+
+  burns.push(transactionHash)
+
+  signaller.burned = total
+  signaller.burns = burns
+  signaller.save()
+}
+
+function storeProposal(event: AnonymousDepositEvent): void {
+  let proposalId = event.params.name
+  let transactionHash = event.transaction.hash.toHex()
+  let proposal = Proposal.load(proposalId)
+
+  if(proposal == null) proposal = new Proposal(proposalId)
+
+  let burns :string[] = proposal.burn as Array<string>
+  let rejections: Option = proposal.reject
+  let approvals: Option = proposal.approve
+
+  if(rejections == null) rejections = new Option(`${proposalId}@no`)
+  if(approvals == null) approvals = new Option(`${proposalId}@yes`)
   if(burners == null) burners = new Array()
 
-  burners.push(hash)
+  if(event.params.choice == "no") storeOption(rejections, event)
+  else if(event.params.choice == "yes") storeOption(approvals, event)
 
-  for(var index = 0; index < burners.length; index++){
-    let transactionHash = burners[index] as string
-    let burn = AnonymousDeposit.load(transactionHash)
+  proposal.sum = event.params.value + proposal.sum
+  proposal.reject = rejections
+  proposal.approve = approvals
+  proposal.burns = burns
+  proposal.save()
+}
 
-    if(burn != null){
-      let value: number = Math.sqrt(legacyNumber(burn.value))
+function storeOption(option: Option, event: AnonymousDepositEvent): void {
+  let contributions = option.contributions
+  let quadratics = option.quadratics
+  let signallers = option.signallers
+  let burn: BigInt = event.params.value
+  let signaller: Bytes = event.params.from
+  let parsed: string = ""
 
-      if(burn.choice == "no") rejectSqrt = value + rejectSqrt
-      else if(burn.choice == "yes") passSqrt = value + passSqrt
-
-      totalValue = legacyNumber(burn.value) + totalValue
-    }
+  if(quadratics.length > 0){
+    let previous: string = quadratics[quadratics.length-1]
+    let quad: number = Math.sqrt(parseInt(previous) + legacyNumber(burn))
+    parsed = quad.toString()
+  } else {
+    let neophyte: number = Math.sqrt(legacyNumber(burn))
+    parsed = neophyte.toString()
   }
 
-  let decline: string = rejectSqrt.toString()
-  let approve: string = passSqrt.toString()
-  let total: string = totalValue.toString()
+  signallers.push(signaller)
+  contributions.push(burn)
+  quadratics.push(parsed)
 
-  quadratics.decline = decline
-  quadratics.approve = approve
-  quadratics.burners = burners
-  quadratics.total = total
-  quadratics.save()
+  option.contributions = contributions
+  option.signallers = signallers
+  option.quadratics = quadratics
+  option.save()
 }
